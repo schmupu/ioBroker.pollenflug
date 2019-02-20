910b891feb599adc9cc77faa26578a43e86b9bfd
schmupu <thorsten@stueben.de>
2019-02-20 16:44:36 +0100

    Update

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 ![Logo](admin/pollenflug.png)
 
 # Pollen risk index
-
-
 [![Travis CI Build Status](https://travis-ci.org/schmupu/ioBroker.pollenflug.svg?branch=master)](https://travis-ci.org/schmupu/ioBroker.pollenflug)
 [![AppVeyor Build Status](https://ci.appveyor.com/api/projects/status/github/schmupu/ioBroker.pollenflug?branch=master&svg=true)](https://ci.appveyor.com/project/schmupu/ioBroker-pollenflug/)
 [![NPM version](http://img.shields.io/npm/v/iobroker.pollenflug.svg)](https://www.npmjs.com/package/iobroker.pollenflug)
@@ -10,16 +8,14 @@
 
 [![NPM](https://nodei.co/npm/iobroker.pollenflug.png?downloads=true)](https://nodei.co/npm/iobroker.pollenflug/)
 
-...
-
-## Install & Configuration
-
 The German Weather Service DWD prepares daily forecasts of the pollen risk index.
 The pollen species are predicted: hazel, alder, ash, birch, grass, rye, mugwort and
 ambrosia for today and tomorrow, on Friday also for the day after tomorrow (Sunday).
 Updated daily in the morning.
 Information on the pollen counties can be found at: https://www.dwd.de/pollenflug
 
+## Install & Configuration
+Requires node.js 8.0 or higher and Admin v3!
 
 ## Changelog
 
@@ -30,7 +26,7 @@ Information on the pollen counties can be found at: https://www.dwd.de/pollenflu
 ## License
 The MIT License (MIT)
 
-Copyright (c) 2018 Thorsten <thorsten@stueben.de> / <https://github.com/schmupu>
+Copyright (c) 2019 Thorsten <thorsten@stueben.de> / <https://github.com/schmupu>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- pollenflug.js
+++ pollenflug.js
@@ -7,9 +7,10 @@
 
 const utils = require('@iobroker/adapter-core');
 const dwd = require(__dirname + '/lib/dwd');
-const request = require('request');
+const request = require('request-promise-native');
 const adapterName = require('./package.json').name.split('.').pop();
 const adapterNodeVer = require('./package.json').engines.node;
+const semver = require('semver');
 
 let adapter;
 
@@ -21,7 +22,7 @@ function startAdapter(options) {
   // *****************************************************************************************************
   // is called when adapter shuts down - callback has to be called under any circumstances!
   // *****************************************************************************************************
-  adapter.on('unload', (callback) => {
+  adapter.on('unload', async (callback) => {
     try {
       adapter.log.info('Closing Adapter');
       callback();
@@ -34,7 +35,7 @@ function startAdapter(options) {
   // *****************************************************************************************************
   // Listen for sendTo messages
   // *****************************************************************************************************
-  adapter.on('message', (msg) => {
+  adapter.on('message', async (msg) => {
    
   });
 
@@ -42,18 +43,81 @@ function startAdapter(options) {
   // is called when databases are connected and adapter received configuration.
   // start here!
   // *****************************************************************************************************
-  adapter.on('ready', () => {
-    main();
+  adapter.on('ready', async () => {
+    await main();
   });
   return adapter;
 }
 
 
+async function createObjects(content) {
+  try {
+    if(content) {
+      for(let i in content) {
+        let region     = content[i].region_id;
+        let partregion = content[i].partregion_id;
+        let deviceid = adapter.namespace + '.region#' + region;
+        let channelid = deviceid + '.partregion#' + partregion;
+        await this.adapter.setObjectNotExistsAsync(deviceid, {
+          type: 'device',
+          common: {
+            name: 'Rules' + region
+          }
+        });
+        await this.adapter.setObjectNotExistsAsync(channelid, {
+          type: 'channel',
+          common: {
+            name: 'Rule ' + partregion
+          }
+        });
+      }
+    }
+  } catch (error) {
+    // 
+  }
+
+
+}
+
+
+function getPollenflugForRegion(data, region) {
+  let dataregion = [];
+  if(data && data.content) {
+    let content = data.content;
+    for(let i in content) {
+      if(content[i].region_id == region) {
+        dataregion.push(content[i]);
+      } 
+    }
+  }
+  return dataregion;
+} 
+
+async function pollenflugRequest() {
+  let result;
+  let url = 'https://opendata.dwd.de/climate_environment/health/alerts/s31fg.json';
+  try {
+    result = await request(url, { method: 'GET', json: true });
+  } catch (error) {
+    adapter.log.error('Error requesting URL ' + url);
+  } 
+  return result;
+}
+
 // *****************************************************************************************************
 // Main
 // *****************************************************************************************************
-function main() {
+async function main() {
   adapter.log.info('Starting Adapter ' + adapter.namespace + ' in version ' + adapter.version);
+  if (!semver.satisfies(process.version, adapterNodeVer)) {
+    adapter.log.error(`Required node version ${adapterNodeVer} not satisfied with current version ${process.version}.`);
+    setTimeout(() => adapter.stop());
+  }
+
+  let result = await pollenflugRequest();
+  let content = getPollenflugForRegion(result, adapter.config.region);
+  await createObjects(content);
+  adapter.log.info(JSON.stringify(content));
 }
 
 // If started as allInOne mode => return function to create instance
