3e33c4848e1f605e687b23cff3fcf381107d20c9
schmupu <thorsten@stueben.de>
2019-02-22 11:59:00 +0100

    Update pictures

--- pollenflug.js
+++ pollenflug.js
@@ -6,7 +6,6 @@
 'use strict';
 
 const utils = require('@iobroker/adapter-core');
-const dwd = require(__dirname + '/lib/dwd');
 const request = require('request-promise-native');
 const adapterName = require('./package.json').name.split('.').pop();
 const adapterNodeVer = require('./package.json').engines.node;
